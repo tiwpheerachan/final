7775ab68dc03c2f5c8b505c356dd1dba66ba5267
safefy-Dev <safe.th@hotmail.com>
2025-05-30 14:58:24 +0700

    login2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 import time, hmac, hashlib
+import os
 
 app = FastAPI()
 
@@ -28,13 +29,20 @@ async def callback(request: Request):
 @app.get("/login")
 async def login():
     partner_id = os.getenv("PARTNER_ID")
-    partner_key = os.getenv("PARTNER_KEY").encode()
+    partner_key = os.getenv("PARTNER_KEY")
     redirect_url = os.getenv("REDIRECT_URL")
+    
+    print("partner_id:", partner_id)
+    print("partner_key:", partner_key)
+    print("redirect_url:", redirect_url)
+
+    if not partner_id or not partner_key or not redirect_url:
+        return {"error": "Missing env variables"}
+
     path = "/api/v2/shop/auth_partner"
     timestamp = int(time.time())
-    
     base_string = f"{partner_id}{path}{timestamp}"
-    sign = hmac.new(partner_key, base_string.encode(), hashlib.sha256).hexdigest()
+    sign = hmac.new(partner_key.encode(), base_string.encode(), hashlib.sha256).hexdigest()
 
     auth_url = f"https://partner.shopeemobile.com{path}?partner_id={partner_id}&timestamp={timestamp}&sign={sign}&redirect={redirect_url}"
     
