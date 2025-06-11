248e74383f3f21c43c44c55f8d7dbc152f57c6d0
Perthtiw <tiw.pheerachan@shd-technology.co.th>
2025-06-11 13:27:07 +0700

    Update latest Shopee integration

--- main.py
+++ main.py
@@ -171,7 +171,6 @@ async def callback(request: Request):
         })
 
 
-# ✅ รัน Local
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
